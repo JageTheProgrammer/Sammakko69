07c1054b5be43dc5b3e0afc9329d527198d3ab11
JageTheProgrammer <131030750+JageTheProgrammer@users.noreply.github.com>
2025-04-30 08:49:21 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Install ffmpeg manually
+# Download and unpack ffmpeg static binary
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar xJ
 mv ffmpeg-*-amd64-static/ffmpeg ./ffmpeg
 chmod +x ./ffmpeg
